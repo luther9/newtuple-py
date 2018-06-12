731fb73adaa9b306a1b3994a0709b627c88b6c11
Luther Thompson <lutheroto@gmail.com>
2018-06-12 05:12:58 -0400

    Remove an irrelevent comment

--- newtuple.py
+++ newtuple.py
@@ -10,9 +10,6 @@ both, so right now, I'm doing both.
 
 Function names are the same as list method names converted to snakeCase. If the
 list method is a special method, we remove the underscores.
-
-All functions require tuple-like arguments to support slicing and addition and
-return a tuple, usually slightly modified from the first argument.
 """
 
 from functools import reduce
